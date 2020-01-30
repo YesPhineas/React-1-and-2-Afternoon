0e460c45cef94a2208a3bf05787cc3563ea55784
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-01-30 16:14:31 -0700

    styled up the card

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -12,17 +12,27 @@ class Display extends React.Component{
 
 
     render(){
+        
         return(
             <div className='lots'>
                 <section className='home-bar'><h2 className='home-txt'>Home</h2></section>
                 <section>
                     <div className='card'>
-                        <div>
-                            <div>{}</div>
-                            <div>From:{}   </div>
-                            <div></div>
+                        <div className='left-info'>
+                            <div className='person-name'>Name</div>
+                            <div className='person-info'>
+                                <div>From:</div>
+                                <div>Job Title:</div>
+                                <div>Employer:</div>
+                            </div>
+                            <div className='person-movies'>
+                                Favorite Movies:
+                                <div>1.</div>
+                                <div>2.</div>
+                                <div>3.</div>
+                            </div>
                         </div>
-                        <div></div>
+                        <div className='num-of-num'>0/0</div>
                     </div>
                 </section>
                 <section className='button'>
