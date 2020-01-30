17a96656f5a9fe9dcc3bb19852f78f2aeb311797
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-01-30 09:03:54 -0700

    some css done

---
+++ src/Components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../index.css'
+
+class Display extends React.Component{
+    render(){
+        return(
+            <div>
+                
+                I AM DISPLAY
+            </div>
+        )
+    }
+}
+
+export default Display
