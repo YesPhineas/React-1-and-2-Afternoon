5ae96f66a66d99a2f3a2891d2101634ffdb0ea9f
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-01-30 19:39:10 -0700

    buttons work

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../index.css'
-import Data from './Data'
-import Buttons from './Buttons'
+import data from './data'
 
 class Display extends React.Component{
     constructor(props){
@@ -9,6 +8,25 @@ class Display extends React.Component{
         
         this.state = {
             index: 0
+        }
+
+        this.prevBtn = this.prevBtn.bind(this)
+        this.nextBtn = this.nextBtn.bind(this)
+    }
+
+    prevBtn(){
+        if(this.state.index === 0){
+            this.setState({index: data.length-1})
+        }else{
+            this.setState({index: this.state.index-1})
+        }
+    }
+
+    nextBtn(){
+        if(this.state.index === data.length-1){
+            this.setState({index: 0})
+        }else{
+            this.setState({index: this.state.index+1})
         }
     }
 
@@ -22,24 +40,25 @@ class Display extends React.Component{
                 <section>
                     <div className='card'>
                         <div className='left-info'>
-                            <div className='person-name'>{`${Data[this.state.index].name.first} ${Data[this.state.index].name.last}`}</div>
+                            <div className='person-name'>{`${data[this.state.index].name.first} ${data[this.state.index].name.last}`}</div>
                             <div className='person-info'>
-                                <div>From: {`${Data[this.state.index].city} ${Data[this.state.index].country}`} </div>
-                                <div>Job Title: {Data[this.state.index].title} </div>
-                                <div>Employer: {Data[this.state.index].employer} </div>
+                                <div>From: {`${data[this.state.index].city} ${data[this.state.index].country}`} </div>
+                                <div>Job Title: {data[this.state.index].title} </div>
+                                <div>Employer: {data[this.state.index].employer} </div>
                             </div>
                             <div className='person-movies'>
                                 Favorite Movies:
-                                <div>1. {Data[this.state.index].favoriteMovies[0]} </div>
-                                <div>2. {Data[this.state.index].favoriteMovies[1]} </div>
-                                <div>3. {Data[this.state.index].favoriteMovies[2]} </div>
+                                <div>1. {data[this.state.index].favoriteMovies[0]} </div>
+                                <div>2. {data[this.state.index].favoriteMovies[1]} </div>
+                                <div>3. {data[this.state.index].favoriteMovies[2]} </div>
                             </div>
                         </div>
-                        <div className='num-of-num'>{Data[this.state.index].id}/{Data.length}</div>
+                        <div className='num-of-num'>{data[this.state.index].id}/{data.length}</div>
                     </div>
                 </section>
                 <section className='button'>
-                    <Buttons/>
+                    <button className='previous' onClick={this.prevBtn} >{`< Previous`}</button>
+                    <button className='next' onClick={this.nextBtn} >{`Next >`}</button>
                 </section>
             </div>
         )
